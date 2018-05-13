b9284042cc9a6d858c88ede32106c37c51d94c08
Maria Lungu <anauna.lungu@gmail.com>
2018-05-13 14:46:57 +0300

    installed material design

---
+++ keep-track/src/app/material.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatIconModule, MatToolbarModule, MatSelectModule, MatSnackBarModule } from '@angular/material';
+import { MatCardModule, MatGridListModule, MatTabsModule, MatSlideToggleModule } from '@angular/material';
+import { MatAutocompleteModule, MatButtonToggleModule, MatCheckboxModule, MatRippleModule } from '@angular/material';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+@NgModule({
+  imports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatRippleModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatExpansionModule
+  ],
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatRippleModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatExpansionModule
+  ]
+})
+
+export class MaterialModule { }

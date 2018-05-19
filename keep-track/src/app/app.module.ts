a02203e89c8ce3685a1ab74205d523ca76b9bc0e
Maria Lungu <anauna.lungu@gmail.com>
2018-05-19 15:08:08 +0300

    KT-16 - create component for dashboard

--- keep-track/src/app/app.module.ts
+++ keep-track/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'boards',
+    component: DashboardComponent,
+    data: {
+      title: 'My boords'
+    }
+  },
+  {
+    path: '',
+    redirectTo: '/boards',
+    data: { title: 'Home' },
+    pathMatch: 'full'
+  }
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
